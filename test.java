66fcee5567754399d5b00c7b65b8b368818925d1
Yishuai Geng <92096545+YishuaiGeng@users.noreply.github.com>
2024-09-26 14:47:47 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,28 @@
 // assert
+
 wassert
+  
+assert (customAssertion instanceof SampleAssertion);
+
+/**
+ * @param indexes the expected column indexes which assert or compare
+ * @return {@link CsvComparisonOptions.Builder}
+ */
+
+boolean authenticated = false;
+if(assertion.isUsePrivateKey()) {
+    String privateKeyText = context.expandVariable(assertion.getPrivateKey(), vars);
+    if(password != null) {
+        authenticated = conn.authenticateWithPublicKey(username, privateKeyText.toCharArray(), password);
+    } else {
+        authenticated = conn.authenticateWithPublicKey(username, privateKeyText.toCharArray(), null);
+    }
+} else  {
+    authenticated = conn.authenticateWithPassword(username, password);
+}
+
+if(!authenticated) {
+    conn.close();
+    audit.logAndAudit(Messages.EXCEPTION_WARNING_WITH_MORE_INFO, "Failed to authenticate with the remote server.");
+    return CustomAssertionStatus.FAILED;
+}
